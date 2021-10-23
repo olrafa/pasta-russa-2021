c68b9aab8cf822274f816f619e357e0e971a7cfd
rafaelrolivares <rafaelrolivares@gmail.com>
2021-10-23 14:43:22 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import cover from './images/pasta_russa_2021_400px.jpg';
+import Spotify from './images/streamings/spotify.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pasta-russa">
+      <img src={cover} className="album-cover" alt="album cover" />
+      <div className="album-info">
+        <div className="album-name">
+          <h2>Pasta Russa</h2>
+          <p>2021</p>
+        </div>
+        <div className="album-links">
+          <img src={Spotify} alt="Spotify" className="stream-link" />
+        </div>
+      </div>
     </div>
   );
 }
