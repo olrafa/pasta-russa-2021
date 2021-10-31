7252362eca3df98b6b82aabbd6c31a7247d10cb1
rafaelrolivares <rafaelrolivares@gmail.com>
2021-10-31 22:12:11 +0100

    Updates mail address

--- src/components/ContactLink.tsx
+++ src/components/ContactLink.tsx
@@ -4,7 +4,7 @@ export const ContactLink = () => {
   const [open, setOpen] = useState(false);
 
   const closedItem = <p>Contato</p>;
-  const openItem = <p>pastarussa.conversa@simplelogin.fr</p>;
+  const openItem = <p>pastarussa@tutanota.com</p>;
 
   return (
     <div className="contact-mail" onClick={() => setOpen((_open) => !_open)}>
